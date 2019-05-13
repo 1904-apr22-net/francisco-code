655f7df16ae92e8ad23725e217cc1268e7b6f7b1
Francisco Guerrero <guerrerof615@gmail.com>
2019-05-12 23:13:57 -0500

    added more week 3 stuff

---
+++ Week 3/RestaurantReviews/RestaurantReviews.DataAccess/Migrations/20190511040210_initial.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantReviews.DataAccess.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "RR");
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant",
+                schema: "RR",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Review",
+                schema: "RR",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RestaurantID = table.Column<int>(nullable: false),
+                    ReviewerName = table.Column<string>(maxLength: 128, nullable: false),
+                    Score = table.Column<int>(nullable: false),
+                    Text = table.Column<string>(maxLength: 2048, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Review_Restaurant",
+                        column: x => x.RestaurantID,
+                        principalSchema: "RR",
+                        principalTable: "Restaurant",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_RestaurantID",
+                schema: "RR",
+                table: "Review",
+                column: "RestaurantID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Review",
+                schema: "RR");
+
+            migrationBuilder.DropTable(
+                name: "Restaurant",
+                schema: "RR");
+        }
+    }
+}
---
+++ Week 3/finalMovieApp/MovieApp/MovieApp.DA/MovieDbRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovieApp.DA
+{
+    public class MovieDbRepository : IMovieRepository
+    {
+        private readonly MovieDbContext _dbContext;
+
+        public MovieDbRepository(MovieDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            return _dbContext.Movie.Include(m => m.Genre).AsNoTracking()
+                .Select(m => new Movie
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    Genre = new Genre { Id = m.Genre.Id, Name = m.Genre.Name },
+                    ReleaseDate = m.ReleaseDate
+                });
+        }
+
+        public IEnumerable<Genre> GetAllGenres()
+        {
+            return _dbContext.Genre.AsNoTracking().Select(g => new Genre
+            {
+                Id = g.Id,
+                Name = g.Name
+            });
+        }
+
+        public Genre GetGenreById(int id)
+        {
+            Entities.Genre entity = _dbContext.Genre.Find(id);
+            return new Genre
+            {
+                Id = entity.Id,
+                Name = entity.Name
+            };
+        }
+
+        public void Create(Movie movie)
+        {
+            var entity = new Entities.Movie
+            {
+                // ignore movie ID if set
+                Title = movie.Title,
+                ReleaseDate = movie.ReleaseDate,
+                Genre = _dbContext.Genre.Find(movie.Genre.Id)
+            };
+
+            _dbContext.Movie.Add(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
